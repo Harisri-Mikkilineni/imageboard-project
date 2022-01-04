3c721c420a5acba19b8e923d44bcc6789c7b3cad
Harisri Mikkilineni <harisri.mikkilineni@gmail.com>
2022-01-04 16:24:44 +0100

    imageboard part2 server.js

--- db.js
+++ db.js
@@ -13,3 +13,10 @@ module.exports.getImages = () => {
     const q = "SELECT * FROM images";
     return db.query(q);
 };
+
+module.exports.addImages = (fileurl, username, title, description) => {
+    const q = `INSERT INTO images (url, username, title, description) 
+    VALUES($1, $2, $3, $4) RETURNING url, username, title, description`;
+    const params = [fileurl, username, title, description];
+    return db.query(q, params);
+};
--- server.js
+++ server.js
@@ -3,26 +3,39 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const { uploader } = require("./upload");
+const s3 = require("./s3");
 
 app.use(express.static("./public"));
 
 app.use(express.json());
 
-app.post("/upload", uploader.single("file"), (req, res) => {
+app.post("/upload", uploader.single("file"), s3.upload, (req, res) => {
     console.log("req.body:", req.body);
     console.log("req.file:", req.file);
-    if (req.file) {
-        res.json({ success: true });
-    } else {
-        res.json({ success: false });
-    }
+
+    const { username, title, description } = req.body;
+    const { filename } = req.file;
+    const fileurl = "https://onionxib.s3.amazonaws.com/" + filename;
+
+    console.log("username:", req.body.username);
+    console.log("title:", req.body.title);
+    console.log("description:", req.body.description);
+    console.log("fileurl:", fileurl);
+
+    db.addImages(fileurl, username, title, description)
+        .then(({ rows }) => {
+            console.log("images added into db:", rows);
+        })
+        .catch((err) => {
+            console.log("error in adding images to db:", err);
+        });
 });
 
 app.get("/images", (req, res) => {
     db.getImages()
         .then(({ rows }) => {
             res.json(rows);
-            console.log("rows:", rows);
+            //console.log("rows:", rows);
         })
         .catch((err) => console.log("err in getting images:", err));
 });
