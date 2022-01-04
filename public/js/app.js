da735382de059f529b020bc75c3758122fc03340
Harisri Mikkilineni <harisri.mikkilineni@gmail.com>
2022-01-04 11:56:51 +0100

    imageboard part1 styling

--- public/js/app.js
+++ public/js/app.js
@@ -4,14 +4,45 @@ Vue.createApp({
     data() {
         return {
             images: [],
+            title: "",
+            description: "",
+            username: "",
+            file: null,
         };
     },
     //api call
     mounted() {
-        fetch("/get-images")
+        fetch("/images")
             .then((resp) => resp.json())
             .then((data) => {
                 this.images = data;
             });
+    },
+    methods: {
+        clickHandler: function () {
+            //console.log("this:", this);
+            const fd = new FormData();
+            fd.append("title", this.title);
+            fd.append("description", this.description);
+            fd.append("username", this.username);
+            fd.append("file", this.file);
+            fetch("/upload", {
+                method: "POST",
+                body: fd,
+            })
+                .then((res) => res.json())
+                .then((result) => {
+                    console.log("result: ", result);
+                    //here
+                })
+                .catch((err) => {
+                    console.log("error uploading new image: ", err);
+                });
+        },
+        fileSelectHandler: function (e) {
+            // console.log("File selected:", e);
+            this.file = e.target.files[0];
+            //now submit these values to server
+        },
     },
 }).mount("#main");
--- server.js
+++ server.js
@@ -2,18 +2,31 @@ const express = require("express");
 
 const app = express();
 const db = require("./db");
+const { uploader } = require("./upload");
 
 app.use(express.static("./public"));
 
 app.use(express.json());
 
-app.get("/get-images", (req, res) => {
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log("req.body:", req.body);
+    console.log("req.file:", req.file);
+    if (req.file) {
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
+
+app.get("/images", (req, res) => {
     db.getImages()
         .then(({ rows }) => {
             res.json(rows);
+            console.log("rows:", rows);
         })
         .catch((err) => console.log("err in getting images:", err));
 });
+
 app.get("*", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
